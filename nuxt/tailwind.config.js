3d4ea70118761e678a01ddd91c2dbbccd994a40e
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-06-04 15:03:36 +0200

    nuxt/tailwind: Purge CSS

--- nuxt/tailwind.config.js
+++ nuxt/tailwind.config.js
@@ -8,4 +8,14 @@ module.exports = {
   theme: {},
   variants: {},
   plugins: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+    ],
+  },
 }
