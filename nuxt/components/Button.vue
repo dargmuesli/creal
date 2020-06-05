a5a14516fb8fd83cb307eee65f50ae4ed4f9df22
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-06-05 17:47:29 +0200

    nuxt/components: Add Button

---
+++ nuxt/components/Button.vue
@@ -0,0 +1,52 @@
+<template>
+  <div :class="wrapperClass">
+    <!-- eslint-disable vue/attribute-hyphenation -->
+    <app-link v-if="link !== undefined" tabIndex="-1" :to="link">
+      <!-- eslint-enable vue/attribute-hyphenation -->
+      <button class="btn" :class="buttonClass" :disabled="disabled">
+        <!-- <font-awesome v-if="icon" class="mr-2" :icon="iconId" /> -->
+        <slot />
+      </button>
+    </app-link>
+    <button v-else class="btn" :class="buttonClass" :disabled="disabled">
+      <!-- <font-awesome v-if="icon" class="mr-2" :icon="iconId" /> -->
+      <slot />
+    </button>
+  </div>
+</template>
+
+<script>
+import AppLink from '~/components/AppLink.vue'
+
+export default {
+  components: { AppLink },
+  props: {
+    buttonClass: {
+      type: String,
+      default: 'bg-yellow-500 inline-block px-6 py-2 text-gray-800',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    // icon: {
+    //   type: Boolean,
+    //   default: true,
+    // },
+    // iconId: {
+    //   type: Array,
+    //   default() {
+    //     return ['fas', 'bug']
+    //   },
+    // },
+    link: {
+      type: String,
+      default: undefined,
+    },
+    wrapperClass: {
+      type: String,
+      default: 'inline-block',
+    },
+  },
+}
+</script>
