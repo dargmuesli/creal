6fac2b46e5f2eefbaef6981c21d90b2f50fc773a
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-06-05 17:47:14 +0200

    nuxt/components: Add AppLink

---
+++ nuxt/components/AppLink.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-bind="linkProps(to)">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    linkProps(url) {
+      if (url.match(/^(http(s)?|ftp):\/\//)) {
+        return {
+          is: 'a',
+          href: url,
+          target: '_blank',
+          rel: 'noopener',
+        }
+      }
+      return {
+        is: 'router-link',
+        to: url,
+      }
+    },
+  },
+}
+</script>
