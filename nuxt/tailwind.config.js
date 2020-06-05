3048ff4756b9666c81ea0071922b269d7d8e2eef
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-06-05 17:50:29 +0200

    nuxt/tailwind: Add Base Style

--- nuxt/tailwind.config.js
+++ nuxt/tailwind.config.js
@@ -7,7 +7,30 @@
 module.exports = {
   theme: {},
   variants: {},
-  plugins: [],
+  plugins: [
+    function ({ addBase, config }) {
+      addBase({
+        body: {
+          background: config('theme.colors.gray.800'),
+          color: config('theme.colors.white'),
+          h1: {
+            fontSize: config('theme.fontSize.6xl'),
+            marginBottom: config('theme.margin.4'),
+            fontWeight: config('theme.fontWeight.bold'),
+          },
+          h2: {
+            fontSize: config('theme.fontSize.4xl'),
+            marginBottom: config('theme.margin.3'),
+            fontWeight: config('theme.fontWeight.bold'),
+          },
+          ':disabled': {
+            cursor: config('theme.cursor.not-allowed'),
+            opacity: config('theme.opacity.50'),
+          },
+        },
+      })
+    },
+  ],
   purge: {
     enabled: process.env.NODE_ENV === 'production',
     content: [
