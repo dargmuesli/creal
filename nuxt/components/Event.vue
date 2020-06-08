29edc3ba79f6310ba993c13da4b5e32b9ea2c071
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-06-08 15:00:46 +0200

    nuxt/event: Responsive

--- nuxt/components/Event.vue
+++ nuxt/components/Event.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="flex">
-    <div class="w-1/2">
+  <div class="items-center flex flex-wrap">
+    <div class="lg:pr-2 lg:w-1/2">
       <div>{{ datetime }}</div>
       <h2 class="m-0">{{ event.title }}</h2>
       <div v-if="event.description !== null" class="mb-4">
         {{ event.description }}
       </div>
-      <div v-if="event.url !== null" class="text-center">
+    </div>
+    <div class="lg:pl-2 lg:w-1/2">
+      <img
+        v-if="event.image !== null"
+        :src="'https://strapi.creal.' + stackDomain + event.image.url"
+        class="mb-4"
+      />
+      <div v-if="event.url !== null" class="mt-4 text-center">
         <Button :link="event.url">Details</Button>
       </div>
     </div>
-    <img
-      v-if="event.image !== null"
-      :src="'https://strapi.creal.' + stackDomain + event.image.url"
-      class="w-1/2"
-    />
   </div>
 </template>
 
