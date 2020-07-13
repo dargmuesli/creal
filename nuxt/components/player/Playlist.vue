bbfdabdddb325598ab3e39cf58a3db0499633d88
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-07-13 18:17:04 +0200

    nuxt/playlist: Retrieve Cover

--- nuxt/components/player/Playlist.vue
+++ nuxt/components/player/Playlist.vue
@@ -1,12 +1,6 @@
 <template>
   <section>
-    <img
-      :src="
-        playlist.coverUrl !== undefined
-          ? playlist.coverUrl
-          : '/player/playlist-cover_default.jpg'
-      "
-    />
+    <img :src="coverUrl" />
     <h1 class="leading-tight m-0 mt-1 text-2xl text-left">
       {{ playlist.name }}
     </h1>
@@ -14,16 +8,42 @@
 </template>
 
 <script lang="ts">
+import merge from 'lodash.mergewith'
 import { Component, Prop, Vue } from 'nuxt-property-decorator'
 
 interface Playlist {
   name: string
-  coverUrl: URL
   items: any[]
 }
 
 @Component({})
 export default class extends Vue {
   @Prop({ type: Object }) readonly playlist!: Playlist
+
+  coverUrl: string = '/player/playlist-cover_default.jpg'
+
+  created() {
+    if ((this.playlist.items as any).cover) {
+      this.getCoverUrl(this.playlist.name)
+    }
+  }
+
+  async getCoverUrl(name: string) {
+    const key = `${
+      this.$route.query.playlist !== undefined ? this.$route.query.playlist : ''
+    }${name}/playlist-cover.jpg`
+    this.coverUrl = await this.$axios.$get('/player/signedUrl', {
+      params: new URLSearchParams(
+        merge(
+          {},
+          {
+            ...(key !== undefined && {
+              key,
+            }),
+          }
+        )
+      ),
+    })
+  }
 }
 </script>
