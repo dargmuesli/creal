6ed4d4556503ee898d9b9d4bf484b01022c4a5d4
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-08-06 14:20:34 +0200

    nuxt/tailwind: Remove Purge Config
    Is added automatically now.

--- nuxt/tailwind.config.js
+++ nuxt/tailwind.config.js
@@ -71,14 +71,4 @@ module.exports = {
       })
     },
   ],
-  purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'plugins/**/*.js',
-      'nuxt.config.js',
-    ],
-  },
 }
