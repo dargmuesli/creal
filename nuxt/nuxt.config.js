0c64991c7111c5a4682c68b0ce3701d35b5cf487
lucasheld <lucasheld@hotmail.de>
2020-10-24 22:20:49 +0200

    feat: add sitemap

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -65,6 +65,7 @@ export default {
     '@nuxtjs/axios',
     '@nuxtjs/proxy',
     'nuxt-fontawesome',
+    '@nuxtjs/sitemap', // Should be declared at the end of the array.
   ],
   /*
    ** Axios module configuration
