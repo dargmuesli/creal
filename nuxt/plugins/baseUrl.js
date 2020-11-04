14c0f9c3d551266d5f91999708bf24b4dea75f3a
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-11-04 04:44:39 +0100

    fix(plugins/base-url): check if file exists

--- nuxt/plugins/baseUrl.js
+++ nuxt/plugins/baseUrl.js
@@ -1,5 +1,7 @@
 import fs from 'fs'
 
+const secretPathAwsBucket = '/run/secrets/creal_aws-bucket'
+
 export const STACK_DOMAIN =
   process.env.NUXT_ENV_STACK_DOMAIN || 'localhost:3000'
 export const BASE_URL = // If NUXT_ENV_STACK_DOMAIN is missing, we assume that a http dev env is used.
@@ -7,8 +9,8 @@ export const BASE_URL = // If NUXT_ENV_STACK_DOMAIN is missing, we assume that a
   '://' +
   STACK_DOMAIN
 export const AWS_BUCKET_NAME =
-  typeof window === 'undefined'
-    ? fs.readFileSync('/run/secrets/creal_aws-bucket', 'utf8')
+  typeof window === 'undefined' && fs.existsSync(secretPathAwsBucket)
+    ? fs.readFileSync(secretPathAwsBucket, 'utf8')
     : ''
 
 export default (_, inject) => {
