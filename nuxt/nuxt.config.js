10c3f89e334ce049a660552ade63f533460ebb97
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-12-27 04:05:12 +0100

    feat: add nuxt html validator

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -38,6 +38,8 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     [
+      // https://go.nuxtjs.dev/typescript
+      '@nuxt/typescript-build',
       '@nuxtjs/fontawesome',
       {
         icons: {
@@ -56,8 +58,7 @@ export default {
         useLayersText: false,
       },
     ],
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    '@nuxtjs/html-validator',
     // Doc: https://github.com/nuxt-community/moment-module
     ['@nuxtjs/moment', { locales: ['de'], plugins: ['twix'] }],
     // https://go.nuxtjs.dev/stylelint
