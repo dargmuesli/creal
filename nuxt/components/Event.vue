226bd3712877332855ea7ffaed95e80a29f23744
Jonas Thelemann <e-mail@jonas-thelemann.de>
2021-10-14 09:12:52 +0200

    fix(event): correct spacing

--- nuxt/components/Event.vue
+++ nuxt/components/Event.vue
@@ -1,14 +1,22 @@
 <template>
   <div
-    class="flex flex-wrap items-center justify-center space-y-4 lg:space-y-0"
+    class="
+      flex flex-col
+      lg:flex-row
+      items-center
+      justify-center
+      lg:space-x-4
+      space-y-4
+      lg:space-y-0
+    "
   >
-    <div class="lg:pr-2 space-y-2 lg:w-1/2">
+    <div class="space-y-2 lg:w-1/2">
       <div>{{ datetime }}</div>
       <h2 class="m-0">{{ event.title }}</h2>
       <!-- eslint-disable-next-line vue/no-v-html -->
       <div v-if="event.description" v-html="$marked(event.description)" />
     </div>
-    <div class="lg:pl-2 space-y-4 lg:w-1/2">
+    <div class="space-y-4 lg:w-1/2">
       <img v-if="event.image" alt="Event image." :src="strapiDomain" />
       <div v-if="event.url && event.url !== ''" class="text-center">
         <Button :aria-label="$t('details')" :icon="false" :link="event.url">
