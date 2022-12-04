7b0d9eda76ba2c4817681c17d333c11374c6783a
Jonas Thelemann <e-mail@jonas-thelemann.de>
2022-12-04 12:17:36 +0100

    fix(postgraphile): correct internal subdomain

--- nuxt/plugins/apollo-config.ts
+++ nuxt/plugins/apollo-config.ts
@@ -1,7 +1,7 @@
 export default () => {
   return {
     httpEndpoint: process.server
-      ? 'http://creal-postgraphile:5000/graphql'
+      ? 'http://creal_postgraphile:5000/graphql'
       : 'https://creal-postgraphile.' +
         (process.env.NUXT_ENV_STACK_DOMAIN || 'jonas-thelemann.test') +
         '/graphql',
