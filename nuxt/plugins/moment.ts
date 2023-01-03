d56667b2c0e98284d52b1127c9f0fb921ba5474d
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-01-03 05:54:12 +0100

    fix(moment): workaround language bug

--- nuxt/plugins/moment.ts
+++ nuxt/plugins/moment.ts
@@ -1,15 +1,17 @@
 import moment from 'dayjs'
-import 'dayjs/locale/de'
-// import duration from 'dayjs/plugin/duration'
+
+// workaround for [1]
+import de from 'dayjs/locale/de'
+// import 'dayjs/locale/de' does not make locale available
+
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
-// import localizedFormat from 'dayjs/plugin/localizedFormat'
-// import relativeTime from 'dayjs/plugin/relativeTime'
 
 export default defineNuxtPlugin((_nuxtApp) => {
-  // moment.extend(duration)
   moment.extend(isSameOrBefore)
-  // moment.extend(localizedFormat)
-  // moment.extend(relativeTime)
+
+  // workaround for [1]
+  moment.locale(de)
+  // moment.locale(en) makes `format` error
 
   return {
     provide: {
@@ -17,3 +19,10 @@ export default defineNuxtPlugin((_nuxtApp) => {
     },
   }
 })
+
+/*
+  [1]
+  https://github.com/nuxt/framework/issues/7534#issuecomment-1248596609
+  https://github.com/nuxt/framework/issues/7206
+  https://github.com/maevsi/maevsi/issues/956
+*/
