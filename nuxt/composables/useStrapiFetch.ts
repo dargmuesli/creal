2763818ddef61f3ad7ee45ff87219652b4d9f256
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-05-17 15:44:37 +0200

    fix(nuxt): use ofetch instead of $fetch

--- nuxt/composables/useStrapiFetch.ts
+++ nuxt/composables/useStrapiFetch.ts
@@ -1,7 +1,10 @@
+import { ofetch } from 'ofetch'
+
 export const useStrapiFetch = () => {
   const getServiceHref = useGetServiceHref()
 
-  return $fetch.create({
+  // TODO: switch back to $fetch (https://github.com/unjs/nitro/issues/470)
+  return ofetch.create({
     baseURL: getServiceHref({ name: 'creal_strapi', port: 1337 }) + '/api',
   })
 }
