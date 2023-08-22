406d751535db63587a0e93fb93c3e2d6c637ec33
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-08-22 04:32:04 +0200

    chore(nuxt): migrate to vio

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -1,12 +1,10 @@
-import {
-  I18N_COOKIE_NAME,
-  LOCALES,
-  SITE_NAME,
-  TIMEZONE_COOKIE_NAME,
-} from './utils/constants'
+import { I18N_MODULE_CONFIG } from '@dargmuesli/nuxt-vio/utils/constants'
+
+import { SITE_NAME } from './utils/constants'
 
 const BASE_URL =
-  'https://creal.' +
+  (process.env.NUXT_PUBLIC_STACK_DOMAIN ? 'https' : 'http') +
+  '://creal.' +
   (process.env.NUXT_PUBLIC_STACK_DOMAIN ||
     `${process.env.HOST || 'localhost'}:${
       !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
@@ -17,52 +15,32 @@ const BASE_URL =
 export default defineNuxtConfig({
   app: {
     head: {
-      htmlAttrs: {
-        lang: 'en', // fallback data to prevent invalid html at generation
-      },
-      titleTemplate: `%s`,
       title: SITE_NAME, // fallback data to prevent invalid html at generation
     },
-    pageTransition: {
-      name: 'layout',
-    },
   },
-  css: ['@/assets/css/main.css'],
-  modules: [
-    '@dargmuesli/nuxt-cookie-control',
-    '@nuxtjs/color-mode',
-    '@nuxtjs/html-validator',
-    '@nuxtjs/i18n',
-    '@pinia/nuxt',
-    'nuxt-seo-kit-module',
-  ],
-  nitro: {
-    compressPublicAssets: true,
-  },
-  postcss: {
-    plugins: { tailwindcss: {}, autoprefixer: {} },
-  },
+  extends: ['@dargmuesli/nuxt-vio'],
   runtimeConfig: {
     public: {
-      googleAnalyticsId: '', // set via environment variable `NUXT_PUBLIC_GOOGLE_ANALYTICS_ID` only
+      creal: {
+        s3: {
+          bucket: 'creal-audio',
+          endpoint: 'https://s3.nl-ams.scw.cloud',
+          region: 'nl-ams',
+        },
+      },
       i18n: {
         baseUrl: BASE_URL,
       },
-      isInProduction: process.env.NODE_ENV === 'production',
-      isTesting: false,
-      s3Bucket: 'creal-audio',
-      s3Endpoint: 'https://s3.nl-ams.scw.cloud',
-      s3Region: 'nl-ams',
-      stagingHost:
-        process.env.NODE_ENV !== 'production' &&
-        !process.env.NUXT_PUBLIC_STACK_DOMAIN
-          ? 'jonas-thelemann.de'
-          : undefined,
+      vio: {
+        stagingHost:
+          process.env.NODE_ENV !== 'production' &&
+          !process.env.NUXT_PUBLIC_STACK_DOMAIN
+            ? 'jonas-thelemann.de'
+            : undefined,
+      },
     },
   },
   typescript: {
-    shim: false,
-    strict: true,
     tsConfig: {
       compilerOptions: {
         esModuleInterop: true,
@@ -73,86 +51,8 @@ export default defineNuxtConfig({
   },
 
   // modules
-  cookieControl: {
-    cookies: {
-      necessary: [
-        {
-          description: {
-            de: 'Dieser Cookie von uns speichert die Einstellungen, die in diesem Dialog getroffen werden.',
-            en: 'This cookie of ours stores the settings made in this dialog.',
-          },
-          id: 'c',
-          name: {
-            de: 'Cookie-Präferenzen',
-            en: 'Cookie Preferences',
-          },
-          targetCookieIds: ['ncc_c', 'ncc_e'],
-        },
-        {
-          description: {
-            de: 'Dieser Cookie von uns speichert die Sprache, in der diese Webseite angezeigt wird.',
-            en: "This cookie of ours stores the language that's used to display this website.",
-          },
-          id: 'l',
-          name: {
-            de: 'Sprache',
-            en: 'Language',
-          },
-          targetCookieIds: [I18N_COOKIE_NAME],
-        },
-        {
-          description: {
-            de: 'Dieser Cookie von uns speichert die Zeitzone, in der sich das Gerät zu befinden scheint.',
-            en: 'This cookie of ours saves the timezone in which the device appears to be located.',
-          },
-          id: 't',
-          name: {
-            de: 'Zeitzone',
-            en: 'Timezone',
-          },
-          targetCookieIds: [TIMEZONE_COOKIE_NAME],
-        },
-      ],
-      optional: [
-        {
-          description: {
-            de: 'Die Cookies vom Drittanbieter Google ermöglichen die Analyse von Nutzerverhalten. Diese Analyse hilft uns unsere Dienste zu verbessern, indem wir verstehen, wie diese Webseite genutzt wird.',
-            en: 'The third-party cookies by Google enable the analysis of user behavior. This analysis helps us to improve our services by understanding how this website is used.',
-          },
-          id: 'ga',
-          links: {
-            'https://policies.google.com/privacy': 'Google Privacy Policy',
-            'https://policies.google.com/terms': 'Google Terms of Service',
-          },
-          name: 'Analytics',
-          targetCookieIds: ['_ga', '_ga_K4R41W62BR'],
-        },
-      ],
-    },
-    locales: ['en', 'de'],
-  },
-  colorMode: {
-    classSuffix: '',
-  },
-  htmlValidator: {
-    failOnError: true,
-    logLevel: 'warning',
-  },
-  i18n: {
-    defaultLocale: 'en', // Must be set for the default prefix_except_default prefix strategy.
-    detectBrowserLanguage: {
-      cookieKey: I18N_COOKIE_NAME,
-      cookieSecure: true,
-    },
-    langDir: 'locales',
-    lazy: true,
-    locales: LOCALES,
-  },
-  seoKit: {
-    splash: false,
-  },
+  i18n: I18N_MODULE_CONFIG, // `langDir`, `lazy` and `locales` must be configured to extend a layer having lazy-loaded translations (https://v8.i18n.nuxtjs.org/guide/layers#locales)
   site: {
-    debug: process.env.NODE_ENV === 'development',
     name: SITE_NAME,
     url: BASE_URL,
   },
