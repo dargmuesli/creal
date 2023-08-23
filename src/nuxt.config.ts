63225c24439e9f26c509eba9c344847c837c091f
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-08-23 05:40:23 +0200

    fix(nuxt): merge configurations deeply

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -1,23 +1,28 @@
 import { VIO_NUXT_BASE_CONFIG } from '@dargmuesli/nuxt-vio/utils/constants'
+import { defu } from 'defu'
 
 import { BASE_URL, SITE_NAME } from './utils/constants'
 
-export default defineNuxtConfig({
-  ...VIO_NUXT_BASE_CONFIG({
-    baseUrl: BASE_URL,
-    siteName: SITE_NAME,
-    stagingHost: 'jonas-thelemann.de',
-  }),
-  extends: ['@dargmuesli/nuxt-vio'],
-  runtimeConfig: {
-    public: {
-      creal: {
-        s3: {
-          bucket: 'creal-audio',
-          endpoint: 'https://s3.nl-ams.scw.cloud',
-          region: 'nl-ams',
+export default defineNuxtConfig(
+  defu(
+    {
+      extends: ['@dargmuesli/nuxt-vio'],
+      runtimeConfig: {
+        public: {
+          creal: {
+            s3: {
+              bucket: 'creal-audio',
+              endpoint: 'https://s3.nl-ams.scw.cloud',
+              region: 'nl-ams',
+            },
+          },
         },
       },
     },
-  },
-})
+    VIO_NUXT_BASE_CONFIG({
+      baseUrl: BASE_URL,
+      siteName: SITE_NAME,
+      stagingHost: 'jonas-thelemann.de',
+    }),
+  ),
+)
