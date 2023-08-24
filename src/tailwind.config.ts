85e5313e61c44060931d4b35db38dbd7a04cd8a4
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-08-24 16:31:38 +0200

    chore(style): set primary color

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from 'tailwindcss'
 import type { PluginAPI } from 'tailwindcss/types/config'
+import colors from 'tailwindcss/colors'
+
+const { gray, yellow } = colors
 
 export default {
+  content: [],
   plugins: [
     ({ addComponents }: PluginAPI) => {
       addComponents({
@@ -11,4 +15,16 @@ export default {
       })
     },
   ],
+  theme: {
+    extend: {
+      colors: {
+        vio: {
+          primary: {
+            bg: yellow['500'],
+            text: gray['900'],
+          },
+        },
+      },
+    },
+  },
 } as Config
