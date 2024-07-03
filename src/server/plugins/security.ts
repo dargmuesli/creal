74bd85a3091055c0af3829d981e8780cd4e99edc
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-07-03 08:29:53 +0200

    chore(deps): update @dargmuesli/nuxt-vio to v13.1.14

--- src/server/plugins/security.ts
+++ src/server/plugins/security.ts
@@ -1,30 +1,6 @@
+import { cleanupCsp } from '@dargmuesli/nuxt-vio/server/plugins/security'
 import { defu } from 'defu'
-import type { NuxtOptions } from 'nuxt/schema'
 import { GET_CSP } from '../../server/utils/constants'
-
-// remove invalid `'none'`s and duplicates // TODO: import from vio
-const cleanupCsp = (
-  nuxtSecurityConfiguration: Partial<NuxtOptions['security']>,
-) => {
-  if (
-    nuxtSecurityConfiguration.headers &&
-    nuxtSecurityConfiguration.headers.contentSecurityPolicy
-  ) {
-    const csp = nuxtSecurityConfiguration.headers.contentSecurityPolicy
-
-    for (const [key, value] of Object.entries(csp)) {
-      if (!Array.isArray(value)) continue
-
-      const valueFiltered = value.filter((x) => x !== "'none'")
-
-      if (valueFiltered.length) {
-        ;(csp as Record<string, unknown>)[key] = [...new Set(valueFiltered)]
-      }
-    }
-  }
-
-  return nuxtSecurityConfiguration
-}
 
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('nuxt-security:routeRules', async (routeRules) => {
