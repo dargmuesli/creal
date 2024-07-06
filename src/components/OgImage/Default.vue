c549492226f636d36b026c2c7a5a332328326120
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-07-06 02:29:41 +0200

    fix(og): correct image description

--- src/components/OgImage/Default.vue
+++ src/components/OgImage/Default.vue
@@ -22,25 +22,26 @@
         margin-left: 64px;
       "
     >
-      <div style="display: flex; align-items: center">
-        <h1 style="font-size: 72px; line-height: 1">{{ title }}</h1>
-        <img
-          alt="DJ cReal"
-          src="/assets/static/logos/creal.svg"
-          width="100"
-          height="100"
-        />
-      </div>
+      <img
+        alt="DJ cReal"
+        src="/assets/static/logos/creal.svg"
+        width="700"
+        height="700"
+        style="position: absolute; opacity: 0.025; left: 100"
+      />
       <div
         style="
           color: rgb(209 213 219);
-          font-size: 30px;
-          line-height: 36px;
-          margin-top: 64px;
+          font-size: 40px;
+          font-weight: 600;
+          margin-top: 80px;
         "
       >
-        {{ description }}
+        {{ t('description') }}
       </div>
+      <h1 style="font-size: 72px; line-height: 1; max-width: 600px">
+        {{ title }}
+      </h1>
     </div>
   </div>
 </template>
@@ -51,6 +52,8 @@ interface Props {
   title: string
 }
 withDefaults(defineProps<Props>(), {})
+
+const { t } = useI18n()
 </script>
 
 <script lang="ts">
@@ -58,3 +61,10 @@ export default {
   name: 'OgImage',
 }
 </script>
+
+<i18n lang="yaml">
+de:
+  description: DJ cReal
+en:
+  description: DJ cReal
+</i18n>
