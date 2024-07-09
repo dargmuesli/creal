31e31efae085fcbc1d18f6d3fe1b7d4db2f9350b
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-07-09 08:21:29 +0200

    fix(layout): automate hyphens

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -61,7 +61,7 @@
     <div class="container mx-auto p-4 md:px-8">
       <div class="flex min-h-screen flex-col pb-32">
         <LayoutHeader />
-        <main class="flex flex-1">
+        <main class="flex flex-1 hyphens-auto">
           <slot />
         </main>
       </div>
