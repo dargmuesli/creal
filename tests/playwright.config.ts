7edf3f1da432fba2d1659584ae74836c0c7bf2fd
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-09-28 23:25:32 +0200

    refactor(jiti): upgrade to v2

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -1,9 +1,9 @@
 // TODO: remove jiti (https://github.com/microsoft/playwright/issues/14303)
-import jiti from 'jiti'
+import { createJiti } from 'jiti'
 
 const moduleFileUrl = new URL(import.meta.url)
-const JITI = jiti(moduleFileUrl.pathname)
-const vioConfiguration = JITI(
+const jiti = createJiti(moduleFileUrl.pathname)
+const vioConfiguration = jiti(
   './node_modules/@dargmuesli/nuxt-vio-testing/playwright.config',
 ).default
 
