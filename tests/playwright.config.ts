a00a1a52893bc1fc0547ad873e088879b8b88b1a
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-09-28 23:27:31 +0200

    refactor(jiti): migrate to esm import

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -1,10 +1,9 @@
 // TODO: remove jiti (https://github.com/microsoft/playwright/issues/14303)
 import { createJiti } from 'jiti'
 
-const moduleFileUrl = new URL(import.meta.url)
-const jiti = createJiti(moduleFileUrl.pathname)
-const vioConfiguration = jiti(
+const jiti = createJiti(import.meta.url, { interopDefault: true })
+const vioConfiguration = await jiti.import(
   './node_modules/@dargmuesli/nuxt-vio-testing/playwright.config',
-).default
+)
 
 export default vioConfiguration
