46b525e88620673b182fc58b4e6f89b24b661aad
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-06-23 11:00:12 +0200

    feat(docker): source secrets as environment variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 set -e
 
+ENVIRONMENT_VARIABLES_PATH="/run/environment-variables"
+
+for environment_variable_file in "$ENVIRONMENT_VARIABLES_PATH"/*; do
+  if [ -f "$environment_variable_file" ]; then
+    environment_variable_name=$(basename "$environment_variable_file")
+    export "$environment_variable_name"="$(cat "$environment_variable_file")"
+  fi
+done
+
 sqitch -C /srv/app/sqitch/ deploy "$(cat /run/secrets/creal_sqitch-target)"
 
 if [ "$NODE_ENV" != "production" ]; then
