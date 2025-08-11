90c658c7876567e6f8a12b7c2a6040d277b6370f
Jonas Thelemann <e-mail@jonas-thelemann.de>
2025-08-11 10:00:42 +0200

    chore(urql): remove comments

--- src/app/plugins/urql.ts
+++ src/app/plugins/urql.ts
@@ -5,65 +5,18 @@ import {
   fetchExchange,
 } from '@urql/core'
 import type { ClientOptions, SSRData } from '@urql/core'
-// import type { Data } from '@urql/exchange-graphcache'
-import {
-  /* Cache, */ offlineExchange as getOfflineExchange,
-} from '@urql/exchange-graphcache'
+import { offlineExchange as getOfflineExchange } from '@urql/exchange-graphcache'
 import { makeDefaultStorage } from '@urql/exchange-graphcache/default-storage'
-// import { relayPagination } from '@urql/exchange-graphcache/extras'
 import { devtoolsExchange } from '@urql/devtools'
 import { provideClient } from '@urql/vue'
 import { consola } from 'consola'
 import { ref } from 'vue'
 
 import schema from '~~/gql/generated/introspection'
-// import type { GraphCacheConfig } from '~~/gql/generated/graphcache'
-// import { cacheExchange } from '@urql/exchange-graphcache'
 
 const SSR_KEY = '__URQL_DATA__'
-// const invalidateCache = (
-//   cache: Cache,
-//   name: string,
-//   args?: { input: { id: any } }
-// ) =>
-//   args
-//     ? cache.invalidate({ __typename: name, id: args.input.id })
-//     : cache
-//         .inspectFields('Query')
-//         .filter((field) => field.fieldName === name)
-//         .forEach((field) => {
-//           cache.invalidate('Query', field.fieldKey)
-//         })
-// TODO: create manual updates that do not require invalidation (https://github.com/maevsi/maevsi/issues/720)
-// const listPush = (cache: Cache, fieldName: string, data: Data | null) =>
-//   cache
-//     .inspectFields('Query')
-//     .filter((field) => field.fieldName === fieldName)
-//     .forEach((field) => {
-//       const dataField = cache.resolve('Query', field.fieldKey)
 
-//       if (typeof dataField !== 'string')
-//         throw new Error('Data field is no string!')
-
-//       const allInvitations = cache.resolve(dataField, 'nodes')
-
-//       if (
-//         !data ||
-//         !Array.isArray(allInvitations) ||
-//         !isNonEmptyArrayOfStrings(allInvitations)
-//       )
-//         throw new Error('Data field is no array!')
-
-// // TODO: insert IDs, not data  (https://github.com/maevsi/maevsi/issues/720)
-//       allInvitations.push(data)
-//       cache.link('Query', field.fieldKey, allInvitations)
-//     })
-
-// const isNonEmptyArrayOfStrings = (value: unknown): value is (string | Data)[] => {
-//   return Array.isArray(value) && value.every((item) => typeof item === 'string')
-// }
-
-export default defineNuxtPlugin((nuxtApp) => {
+export default defineNuxtPlugin(async (nuxtApp) => {
   const runtimeConfig = useRuntimeConfig()
   const getServiceHref = useGetServiceHref()
 
@@ -82,40 +35,6 @@ export default defineNuxtPlugin((nuxtApp) => {
       nuxtApp.payload[SSR_KEY] = ssrExchange.extractData()
     })
   }
-
-  // const cacheConfig: GraphCacheConfig = {
-  //   schema,
-  //   // resolvers: {
-  //   //   Query: {
-  //   //     allContacts: relayPagination(),
-  //   //     allEvents: relayPagination(),
-  //   //     allInvitations: relayPagination(),
-  //   //     allUploads: relayPagination(),
-  //   //   },
-  //   // },
-  //   storage: makeDefaultStorage(),
-  //   // updates: {
-  //   //   Mutation: {
-  //   //     // create
-  //   //     createContact: (_parent, _args, cache, _info) =>
-  //   //       invalidateCache(cache, 'allContacts'),
-  //   //     createInvitation: (_parent, _args, cache, _info) =>
-  //   //       invalidateCache(cache, 'allInvitations'),
-  //   //     // TODO: create manual updates that do not require invalidation (https://github.com/maevsi/maevsi/issues/720)
-  //   //     // listPush(cache, 'allInvitations', parent.createInvitation),
-
-  //   //     // update
-  //   //     profilePictureSet: (_parent, _args, cache, _info) =>
-  //   //       invalidateCache(cache, 'profilePictureByUsername'),
-
-  //   //     // delete
-  //   //     deleteContactById: (_parent, args, cache, _info) =>
-  //   //       invalidateCache(cache, 'Contact', args),
-  //   //     deleteInvitationById: (_parent, args, cache, _info) =>
-  //   //       invalidateCache(cache, 'Invitation', args),
-  //   //   },
-  //   // },
-  // }
 
   const cacheExchange = import.meta.client
     ? getOfflineExchange({
@@ -146,7 +65,7 @@ export default defineNuxtPlugin((nuxtApp) => {
     exchanges: [
       ...(runtimeConfig.public.vio.isInProduction ? [] : [devtoolsExchange]),
       ...(cacheExchange ? [cacheExchange] : []),
-      ssrExchange, // `ssr` must be before `fetchExchange`
+      ssrExchange, // `ssrExchange` must be before `fetchExchange`
       fetchExchange,
     ],
   }
@@ -158,31 +77,6 @@ export default defineNuxtPlugin((nuxtApp) => {
     const { $urql } = useNuxtApp()
     provideClient($urql)
   })
-
-  // // Either authenticate anonymously or refresh token on page load.
-  // if (nuxtApp.ssrContext?.event) {
-  //   const store = useStore(nuxtApp.ssrContext.$pinia)
-  //   const jwtFromCookie = getJwtFromCookie({
-  //     req: nuxtApp.ssrContext.event.node.req,
-  //   })
-
-  //   if (jwtFromCookie?.jwtDecoded.id) {
-  //     await jwtRefresh({
-  //       client: client.value,
-  //       $urqlReset: urqlReset,
-  //       store,
-  //       res: nuxtApp.ssrContext.event.node.res,
-  //       id: jwtFromCookie.jwtDecoded.id as string,
-  //     })
-  //   } else {
-  //     await authenticationAnonymous({
-  //       client: client.value,
-  //       $urqlReset: urqlReset,
-  //       store,
-  //       res: nuxtApp.ssrContext.event.node.res,
-  //     })
-  //   }
-  // }
 
   return {
     provide: {
