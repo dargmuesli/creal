25720292cdecd3caa5511fb9d6bc68bfd0186409
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-01-30 05:49:38 +0100

    feat(contact): migrate contact form to vio (#603)

--- src/server/utils/constants.ts
+++ src/server/utils/constants.ts
@@ -1,5 +1,3 @@
-import { getRootHost } from '@dargmuesli/nuxt-vio/shared/utils/networking'
-import { defu } from 'defu'
 import { parseURL } from 'ufo'
 
 export const DARGSTACK_SECRET_UNUSED_THIRD_PARTY = 'UNSET THIRD PARTY SECRET'
@@ -11,35 +9,28 @@ export const GET_CSP = ({ siteUrl }: { siteUrl: URL }) => {
     ? PRODUCTION_HOST
     : getRootHost(siteUrl.host)
 
-  return defu(
-    {
-      // creal
-      'connect-src': [
-        `https://backend.${domainTldPort}/api/`, // contact form
-        `https://creal-postgraphile.${domainTldPort}`,
-        `https://creal-strapi.${domainTldPort}`,
-        'https://cdn.plyr.io', // plyr
-      ],
-      'form-action': ["'self'"],
-      'img-src': [
-        `https://creal-strapi.${domainTldPort}`,
-        `https://${crealS3EndpointHost}`, // playlist cover
-      ],
-      'media-src': [
-        'https://cdn.plyr.io/static/blank.mp4', // plyr
-        `https://${crealS3EndpointHost}`, // music
-      ],
-      'report-to': 'csp-endpoint',
-      'report-uri':
-        'https://o4507259039973376.ingest.de.sentry.io/api/4507260561653840/security/?sentry_key=1e53178c1dba9b39147de4a21853a3e3',
-      'style-src': [
-        "'unsafe-inline'", // TODO: remove when Nuxt loading indicator and headlessui (Dialog) work without it
-      ],
-    },
-    {
-      // Cloudflare Turnstile
-      'frame-src': ['https://challenges.cloudflare.com'],
-      'script-src-elem': ['https://challenges.cloudflare.com'],
-    },
-  )
+  return {
+    // creal
+    'connect-src': [
+      `https://backend.${domainTldPort}/api/`, // contact form
+      `https://creal-postgraphile.${domainTldPort}`,
+      `https://creal-strapi.${domainTldPort}`,
+      'https://cdn.plyr.io', // plyr
+    ],
+    'form-action': ["'self'"],
+    'img-src': [
+      `https://creal-strapi.${domainTldPort}`,
+      `https://${crealS3EndpointHost}`, // playlist cover
+    ],
+    'media-src': [
+      'https://cdn.plyr.io/static/blank.mp4', // plyr
+      `https://${crealS3EndpointHost}`, // music
+    ],
+    'report-to': 'csp-endpoint',
+    'report-uri':
+      'https://o4507259039973376.ingest.de.sentry.io/api/4507260561653840/security/?sentry_key=1e53178c1dba9b39147de4a21853a3e3',
+    'style-src': [
+      "'unsafe-inline'", // TODO: remove when Nuxt loading indicator and headlessui (Dialog) work without it
+    ],
+  }
 }
