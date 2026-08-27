9d31a94c5f9a19377176d279dc0770c8d2f43570
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-08-27 13:07:27 +0200

    feat(sentry): use vio's built-in Sentry integration

--- src/server/utils/constants.ts
+++ src/server/utils/constants.ts
@@ -1,3 +1,4 @@
+import { defu } from 'defu'
 import { parseURL } from 'ufo'
 
 const crealS3EndpointHost = `${process.env.NUXT_PUBLIC_CREAL_S3_BUCKET}.${parseURL(process.env.NUXT_PUBLIC_CREAL_S3_ENDPOINT).host}`
@@ -6,30 +7,42 @@ export const GET_CSP = ({ siteUrl }: { siteUrl: URL }) => {
   const domainTldPort = IS_IN_FRONTEND_DEVELOPMENT
     ? PRODUCTION_HOST
     : getRootHost(siteUrl.host)
+  const sentryConfig = useRuntimeConfig().public.sentry
 
-  return {
-    // creal
-    'connect-src': [
-      `https://backend.${domainTldPort}/api/`, // contact form
-      `https://creal-postgraphile.${domainTldPort}`,
-      `https://creal-strapi.${domainTldPort}`,
-      'https://cdn.plyr.io', // plyr
-    ],
-    'font-src': ["'self'"], // og-image
-    'form-action': ["'self'"],
-    'img-src': [
-      `https://creal-strapi.${domainTldPort}`,
-      `https://${crealS3EndpointHost}`, // playlist cover
-    ],
-    'media-src': [
-      'https://cdn.plyr.io/static/blank.mp4', // plyr
-      `https://${crealS3EndpointHost}`, // music
-    ],
-    'report-to': 'csp-endpoint',
-    'report-uri':
-      'https://o4507259039973376.ingest.de.sentry.io/api/4507260561653840/security/?sentry_key=1e53178c1dba9b39147de4a21853a3e3',
-    'style-src': [
-      "'unsafe-inline'", // TODO: remove when Nuxt loading indicator and headlessui (Dialog) work without it
-    ],
-  }
+  return defu(
+    {
+      // creal
+      'connect-src': [
+        `https://backend.${domainTldPort}/api/`, // contact form
+        `https://creal-postgraphile.${domainTldPort}`,
+        `https://creal-strapi.${domainTldPort}`,
+        'https://cdn.plyr.io', // plyr
+      ],
+      'font-src': ["'self'"], // og-image
+      'form-action': ["'self'"],
+      'img-src': [
+        `https://creal-strapi.${domainTldPort}`,
+        `https://${crealS3EndpointHost}`, // playlist cover
+      ],
+      'media-src': [
+        'https://cdn.plyr.io/static/blank.mp4', // plyr
+        `https://${crealS3EndpointHost}`, // music
+      ],
+      'report-to': 'sentry',
+      'style-src': [
+        "'unsafe-inline'", // TODO: remove when Nuxt loading indicator and headlessui (Dialog) work without it
+      ],
+    },
+    {
+      // Sentry (vio only wires this in automatically for static builds)
+      ...(sentryConfig.host && sentryConfig.project.id
+        ? {
+            'connect-src': [
+              `https://${sentryConfig.host}/api/${sentryConfig.project.id}/envelope/`,
+            ],
+            'worker-src': ['blob:'],
+          }
+        : {}),
+    },
+  )
 }
